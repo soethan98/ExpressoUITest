81bae25657c44eb401f44f937c76513d0f5b3764
Soe Than <soethan@better.hr>
2020-07-14 21:45:10 +0630

    Activity in isolation

---
+++ app/src/androidTest/java/com/example/androidxuitest/SecondActivityTest.kt
@@ -0,0 +1,42 @@
+package com.example.androidxuitest
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class SecondActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(SecondActivity::class.java)
+
+    @Test
+    fun testActivity_inView() {
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+    // Visibility
+    @Test
+    fun testVisibility_title_nextButton() {
+        onView(withId(R.id.activity_secondary_title))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back))
+            .check(matches(isDisplayed()))
+    }
+
+    // Text
+    @Test
+    fun testTitleTextDisplayed() {
+        onView(withId(R.id.activity_secondary_title))
+            .check(matches(withText(R.string.text_secondaryactivity)))
+    }
+}

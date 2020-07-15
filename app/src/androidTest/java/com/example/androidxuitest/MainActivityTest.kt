f7bb36d75416c88cbc5eab031e670222cf82e5ce
Soe Than <soethan@better.hr>
2020-07-15 10:30:31 +0630

    activity-navigation

--- app/src/androidTest/java/com/example/androidxuitest/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/androidxuitest/MainActivityTest.kt
@@ -2,26 +2,33 @@ package com.example.androidxuitest
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
 
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
     fun testActivity_inView() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 
     //Visibilty
     @Test
     fun testVisibility_title_nextButton() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+//        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.activity_main_title)).check(matches(isDisplayed())) //method 1
 
         onView(withId(R.id.activity_main_title)).check(matches(withEffectiveVisibility(Visibility.VISIBLE))) //method 2
@@ -33,9 +40,34 @@ class MainActivityTest {
     //Text
     @Test
     fun testTitleTextDisplayed() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+//        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
         onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)))
+
+    }
+
+    @Test
+    fun test_navSecondaryActivity() {
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Test both ways to navigate from SecondaryActivity to MainActivity
+     */
+    @Test
+    fun test_backPress_toMainActivity() {
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back)).perform(click()) // method 1
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+        pressBack()
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
 
     }
 

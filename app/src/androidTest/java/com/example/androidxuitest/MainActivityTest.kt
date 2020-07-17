1b79dc3878a549422ee7943900d199dbec360d9e
Soe Than <soethan@better.hr>
2020-07-17 10:29:17 +0630

    dialog-capture-input

---
+++ app/src/androidTest/java/com/example/androidxuitest/MainActivityTest.kt
@@ -0,0 +1,45 @@
+package com.example.androidxuitest
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @Test
+    fun test_showDialog_captureNameInput() {
+
+        // GIVEN
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val NAME = "Mitch"
+
+        // Execute and Verify
+        onView(withId(R.id.button_launch_dialog)).perform(click())
+
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        onView(withText(R.string.text_ok)).perform(click())
+        onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
+
+        // enter a name
+        onView(withId(R.id.md_input_message)).perform(typeText(NAME))
+        onView(withText(R.string.text_ok)).perform(click())
+
+        // make sure dialog is gone
+
+        //doesNotExist() to check visible/gone
+        onView(withText(R.string.text_enter_name)).check(doesNotExist())
+        onView(withId(R.id.text_name)).check(matches(withText(NAME)))
+
+
+    }
+}
